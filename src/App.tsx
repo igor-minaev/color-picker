bd7244448d03bfb86a4ede4c9e4800bae53136d6
Igor Minayeu <igor.minayeu@gmail.com>
2023-06-10 16:50:48 +0300

    color-picker with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React, {useState} from 'react';
 import './App.css';
+import {HexColorPicker} from "react-colorful";
+import styled from "styled-components";
 
 function App() {
-    const [color, setColor] = useState('#fff')
+    const [color, setColor] = useState('#111')
     return (
         <div className="App">
-
+            <HexColorPicker className="color-picker" color={color} onChange={setColor}/>
+            <DivWithChoseColor color={color}></DivWithChoseColor>
         </div>
     );
 }
 
 export default App;
+
+
+
+const DivWithChoseColor = styled.div`
+  margin: 30px auto;
+  width: 600px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: ${props => props.color};
+
+`
